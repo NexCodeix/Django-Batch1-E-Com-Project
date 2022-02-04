b69cf49c3776f17af6f86d0191e842858a5debc8
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-02-04 16:12:32 +0600

    created the models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,41 @@
+import secrets
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+User = get_user_model()
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    description = models.TextField()
+    price = models.FloatField()
+    slug = models.SlugField(unique=True)
+
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        slug = self.slug
+        if (slug is None) or (slug == ""):
+            self.slug = slugify(self.name) + str(secrets.token_hex(6))
+
+        return super().save(*args, **kwargs)
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order_items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="ordered")
+    quantity = models.PositiveIntegerField(default=1)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        quantity = self.quantity
+        if quantity and (quantity < 1):
+            raise ValueError("Quantity cannot be 0")
+
+        return super().save(*args, **kwargs)
