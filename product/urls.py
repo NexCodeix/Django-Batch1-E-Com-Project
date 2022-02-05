a20d0a982ace07362dfe2fcfab5f7e017208f1e8
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-02-05 22:56:54 +0600

    inserted all the pages to projects

--- product/urls.py
+++ product/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
+    path("products/", views.shop),
+    path("products/detail/", views.product_detail),
+    path("checkout/", views.checkout),
 ]
--- product/views.py
+++ product/views.py
@@ -5,3 +5,17 @@ def index(request):
 
     return render(request, "product/index.html")
 
+
+def shop(request):
+
+    return render(request, "product/shop.html")
+
+
+def product_detail(request):
+
+    return render(request, "product/product_detail.html")
+
+
+def checkout(request):
+
+    return render(request, "product/checkout.html")
