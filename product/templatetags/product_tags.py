88e269db7c2e100efac4442d29418cf0fe54ecc5
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-03-09 23:50:53 +0600

    added template tags

---
+++ product/templatetags/product_tags.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+def order_items_of_user(user_obj):
+    if not user_obj.is_authenticated:
+        return 0
+        
+    qs = user_obj.orders.filter(submitted=False)
+    if not qs.exists():
+        return 0
+
+    order_obj = qs.get()
+    total = 0
+    order_itm_qs = order_obj.order_items.all()
+
+    for order_itm in order_itm_qs:
+        total += order_itm.quantity
+
+    return total
+
+
+register.filter(name="order_items_of_user", filter_func=order_items_of_user)
--- product/views.py
+++ product/views.py
@@ -24,7 +24,6 @@ class HomePage(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context)
         return context
 
 
