9d8218411cd43b754d9361ebe5127cc1672b2d38
MdSuhaibSafwan <suhaibsafwan45@gmail.com>
2022-03-17 00:35:04 +0600

    nc

--- product/admin.py
+++ product/admin.py
@@ -1,17 +1,11 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Product, ProductImages, Order, OrderItem, Tag, BillingAddress
-=======
 from .models import Product, ProductImages, Order, OrderItem, Tag,Subscribe
->>>>>>> d8fb91a4684bf5e71b098b18b37a996eb9dd1148
 
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(ProductImages)
 admin.site.register(Tag)
-<<<<<<< HEAD
 admin.site.register(BillingAddress)
-=======
 admin.site.register(Subscribe)
->>>>>>> d8fb91a4684bf5e71b098b18b37a996eb9dd1148
